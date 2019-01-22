a8087941c98690b88e1205cf163c7499690da957
Arina <hadson.rey@mail.ru>
2019-01-23 03:04:20 +1100

    Добавляет первый слайдер на главную страницу.

---
+++ js/slider.js
@@ -0,0 +1,27 @@
+var buttonNext = document.querySelector(".slider .slider-controls-next button");
+var buttonPrev = document.querySelector(".slider .slider-controls-prev button");
+
+var sliderList = document.querySelector('.slider-list');
+var total = sliderList.querySelectorAll('li').length;
+
+
+var currentIndex = 0;
+
+function translateX(sliderList, currentIndex) {
+  var width = sliderList.offsetWidth;
+  sliderList.style.transform = 'translateX(' + -width * currentIndex + 'px)';
+}
+
+buttonNext.addEventListener('click', function (event) {
+  if (currentIndex + 1 < total) {
+    currentIndex += 1;
+    translateX(sliderList, currentIndex)
+  }
+});
+
+buttonPrev.addEventListener('click', function (event) {
+  if (currentIndex > 0) {
+    currentIndex -= 1;
+    translateX(sliderList, currentIndex)
+  }
+});
